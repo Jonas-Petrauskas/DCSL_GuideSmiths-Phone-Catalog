2bc1b95eed0789f582b66fddc8def16bda9bc60a
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2021-12-04 13:51:15 +0100

    routes added to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import AddPhone from "./Components/AddPhone/AddPhone";
+import NavBar from "./Components/NavBar/NavBar";
+import PhoneDetails from "./Components/PhoneDetails/PhoneDetails";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <Router>
+      <div className="app">
+        <div className="navbar">
+          <NavBar />
+        </div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/add-phone" component={AddPhone} />
+          <Route exact path="/phones/:id" component={PhoneDetails} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
